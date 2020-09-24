287a0e8efd13115566f3b3fd23caddb9dab730d6
ahviplc <ahlc@sina.cn>
2020-09-24 11:03:06 +0800

    add 【express utility】modules and update helloworld.js and change the project structure.

--- com.app/helloworld.js
+++ com.app/helloworld.js
@@ -3,3 +3,24 @@ console.log("hello world node js")
 console.log("hello world node js")
 console.log("hello world node js")
 console.log("hello world node js")
+console.log("---------------------------------------------------------------------")
+// utility库
+const utils = require('utility')
+console.log(utils.md5('admin'))
+console.log(utils.sha1('admin'))
+console.log(utils.sha256('admin'))
+console.log(utils.sha256(Buffer.from('admin')))
+console.log(utils.hmac('sha1', 'I am a key', 'hello world'))
+
+// base64 encode
+console.log(utils.base64encode('你好￥')); // '5L2g5aW977+l'
+console.log(utils.base64decode('5L2g5aW977+l')) // '你好￥'
+// urlsafe base64 encode
+console.log(utils.base64encode('你好￥', true)); // '5L2g5aW977-l'
+console.log(utils.base64decode('5L2g5aW977-l', true)); // '你好￥'
+// html escape and unescape
+console.log(utils.escape('<script/>"& &amp;')); // '&lt;script/&gt;&quot;&amp; &amp;amp;'
+console.log(utils.unescape('&lt;script/&gt;&quot;&amp; &amp;amp;')); // '<script/>"& &amp;'
+// Safe encodeURIComponent and decodeURIComponent
+console.log(utils.decodeURIComponent(utils.encodeURIComponent('你好, nodejs')));
+console.log("---------------------------------------------------------------------")
